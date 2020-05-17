17617288e1621cf2d1f72522627d157b5f853cb5
afinesami <samibenomar84@gmail.com>
2020-05-17 03:57:13 +0000

    1-hbnb.py

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -6,37 +6,39 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/1-hbnb/')
-def hbnb_filters(the_id=None):
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
     """ HBNB is alive! """
-    states_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-                 for user in storage.all('User').values())
-    return render_template('1-hbnb.html',
-                           states=states,
-                           amens=amens,
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
                            places=places,
-                           users=users,
                            cache_id=uuid.uuid4())
 
 
