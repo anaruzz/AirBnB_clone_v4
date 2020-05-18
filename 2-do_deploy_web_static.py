f9ad7aacfc243b94f57357c583dc057bd6eceb0e
anaruzz <laabidigh@gmail.com>
2020-05-18 20:14:09 +0100

    ok

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
+Fabric script that is based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
