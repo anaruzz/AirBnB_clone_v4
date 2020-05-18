1391a72070f881fccf08bb12b09de18a645045cf
afinesami <samibenomar84@gmail.com>
2020-05-18 01:26:01 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,70 @@
 #!/usr/bin/python3
-""" Flask Application """
-from models import storage
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, url_for, make_response, jsonify
-from flask_cors import cross_origin, CORS
+from flask import Flask, jsonify, make_response, render_template, url_for
+from flask_cors import CORS, cross_origin
 from flasgger import Swagger
-from flasgger.utils import swag_from
+from models import storage
 import os
 from werkzeug.exceptions import HTTPException
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 swagger = Swagger(app)
+
+# global strict slashes
 app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+
+# Cross-Origin Resource Sharing
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """ Method calls .Close current SQLAchemy Session """
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
 
 
 @app.errorhandler(Exception)
 def global_error_handler(err):
     """
-        description: a resource was not found
+        Global Route to handle All Error Status Codes
     """
     if isinstance(err, HTTPException):
         if type(err).__name__ == 'NotFound':
@@ -49,6 +83,7 @@ def setup_global_errors():
     """
     for cls in HTTPException.__subclasses__():
         app.register_error_handler(cls, global_error_handler)
+
 
 if __name__ == "__main__":
     """
